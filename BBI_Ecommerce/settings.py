057c767001addcee31a6bab591a4875de1e68f1a
Silivanxay Phetsouvanh <silivanxay.phetsouvanh@stengg.com>
2022-06-08 10:45:05 +0700

    Fixed bug images not loaded

--- BBI_Ecommerce/settings.py
+++ BBI_Ecommerce/settings.py
@@ -215,7 +215,7 @@ DATABASES = {
         # Tenant Engine
         'ENGINE': 'django_tenants.postgresql_backend',
         # set database name
-        'NAME': 'bbi_ecomm_oscar',
+        'NAME': 'bbi_ecomm',
         # set your user details
         'USER': 'postgres',
         'PASSWORD': 'welcome0',
@@ -274,7 +274,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -292,8 +300,6 @@ OSCARAPI_BLOCK_ADMIN_API_ACCESS = False
 OSCAR_THUMBNAILER = 'oscar.core.thumbnails.EasyThumbnails'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_FINDERS = DEFAULT_SETTINGS.STATICFILES_FINDERS + [
     'compressor.finders.CompressorFinder',
--- BBI_Ecommerce/urls.py
+++ BBI_Ecommerce/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.apps import apps
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -47,3 +49,6 @@ urlpatterns = [
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
