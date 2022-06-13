70032ec603590385d7e0d00f4f0ee01991bb870c
Silivanxay Phetsouvanh <silivanxay.phetsouvanh@stengg.com>
2022-06-13 17:38:29 +0700

    Added upload image from file api.

--- uploadimage/models.py
+++ uploadimage/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Image(models.Model):
-    image = models.ImageField(blank=False, null=False)
+    image = models.FileField(blank=False, null=False)
     def __str__(self):
         return self.image.name
